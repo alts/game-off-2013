65ffd48e266aa5c2a02a2fe6b84fb8b54476c191
Stephen Altamirano <stephen@evilrobotstuff.com>
2013-11-15 01:19:04 -0800

    Add new level

--- levels.js
+++ levels.js
@@ -1,9 +1,9 @@
 (function(){
   var standard_walls = [
     1, 1, 22, 0,
-    1, 2, 0, 14,
-    2, 16, 21, 0,
-    23, 2, 0, 13
+    1, 2, 0, 12,
+    2, 14, 21, 0,
+    23, 2, 0, 11
   ];
 
   return [
@@ -30,6 +30,20 @@
       walls: standard_walls,
       win: function(world) {
         return world.player_character.num_consumed('~food') > 1;
+      }
+    },
+
+    {
+      title: 'NEEDS SOME FRESH AIR',
+      objs: [
+        ['~wizard', 2, 3],
+        ['~food', 7, 2]
+      ],
+      walls: standard_walls,
+      win: function(world) {
+        var player = world.player_character;
+        return (player.x == 0 || player.x == 24 ||
+                player.y == 0 || player.y == 15);
       }
     }
   ];
